40a3d95ddd69b6c5bea26818087f036fb4e88e62
Luke Lloyd <lukelloyd4273@gmail.com>
2023-04-06 22:34:01 +0100

    stars

--- Assets/Scripts/Managers/LevelAvailability.cs
+++ Assets/Scripts/Managers/LevelAvailability.cs
@@ -7,25 +7,18 @@ using UnityEngine.UI;
 public class LevelAvailability : MonoBehaviour
 {
     public List<GameObject> levels;
+	public Sprite goldStar;
+	public Sprite silverStar;
+	public Sprite bronzeStar;
 
 	private void Start()
 	{
 		for (int i = 1; i < LevelController.nextlevel; i++)
 		{
-			GetChild(levels[i - 1], "Image (" + (i) + ")").SetActive(false);
+			GetChild(levels[i - 1], "Image (" + i + ")").SetActive(false);
+			GetChild(levels[i - 1], "Time (" + i + ")").GetComponent<TextMeshProUGUI>().text = LevelController.timeList[i - 1];
+			GetChild(GetChild(levels[i - 1], "StarContainer"), "StarG (" + i + ")").GetComponent<Image>().sprite = goldStar;
 			levels[i - 1].GetComponent<Button>().interactable = true;
-		}
-
-		//for (int i = 0; i < LevelController.currentlevel - 1; i++)
-		//{
-		//	if (i == LevelController.currentlevel - 1)
-		//	{
-		//		GetChild(levels[i], "Time (" + (i + 1) + ")").GetComponent<TextMeshProUGUI>().text = LevelController.lastTime;
-		//	}
-		//}
-
-		for (int j = 1; j < LevelController.nextlevel; j++){
-			GetChild(levels[j - 1], "Time (" + (j) + ")").GetComponent<TextMeshProUGUI>().text = LevelController.timeList[j - 1];
 		}
 		
 	}
